b2274c7dc9b19b356be7e6dbfdf0de9cdd8dbace
Mattaqua <140160310+Mattaqua@users.noreply.github.com>
2023-07-31 21:37:25 +0200

    Add files via upload

---
+++ Les1.py
@@ -0,0 +1,9 @@
+mijn_naam = input("Wat is je naam?")
+print(mijn_naam)
+mijn_geboorteplaats = input("Wat is je geboorteplaats?")
+print(mijn_geboorteplaats)
+mijn_leeftijd = int(input("Wat is je leeftijd?"))
+if mijn_leeftijd >= 50:
+    print("Jantje komt thuis en zegt tegen vader: Papa, ik wil later rijk worden. Dan wil ik een groot huis zonder badkamer en elf bedienden en veel eten natuurlijk. Geen badkamer?, vraagt vader. Neen, antwoordt Jantje, want ik wil stinkend rijk worden.")
+else:
+    print("Sorry, geen grapje voor jou.")
